02d6ebdf570613ac86a624ae9344c3ecd8a8ac40
Raghwendra Kumar <raghwendra.kumar@starhealth.in>
2025-11-09 17:29:43 +0530

    integrated get api in home page and created api routes for details event page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,15 @@
 import Image from "next/image";
 import ExploreBtn from "@/components/ExploreBtn";
 import EventCard from "@/components/EventCard";
-import events from "@/lib/constants";
+import {IEvent} from "@/database";
+// import events from "@/lib/constants";
 
-export default function Home() {
-  return (
+const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
+
+export default async function Home() {
+     const response= await fetch(`${BASE_URL}/api/events`);
+     const {events} = await response.json();
+    return (
   <section>
       <h1 className="text-center">The Hub for Every Dev <br /> Event You Can't Miss</h1>
       <p className="text-center mt-5">Hackathons, Meetups, and Conferences, All in One Place</p>
@@ -15,7 +20,7 @@ export default function Home() {
           <h3>Featured Events</h3>
 
           <ul className="events">
-              {events.map((event)=>(
+              {events && events.length >0 && events.map((event : IEvent)=>(
                   <li key={event.title}>
                       <EventCard {...event}
                       />

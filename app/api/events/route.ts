32469c4f721ae6b0b20f086e8b59ed8aec3c8e22
Raghwendra Kumar <raghwendra.kumar@starhealth.in>
2025-11-09 16:58:37 +0530

    Added cloudinary to upload image directly and created get route to fetch all eevents created from db

--- app/api/events/route.ts
+++ app/api/events/route.ts
@@ -1,6 +1,8 @@
 import {NextRequest,NextResponse} from "next/server";
+import { v2 as cloudinary } from 'cloudinary';
 import connectDB from "@/lib/mongodb";
 import Event from '@/database/event.model'
+
 export async function POST(req:NextRequest)
 {
     try{
@@ -16,11 +18,42 @@ export async function POST(req:NextRequest)
      catch(e){
        return NextResponse.json({ message:'Invalid Json Data format'} ,{status:400})
      }
+
+     const file = formData.get('image') as File;
+
+     if(!file){
+         return NextResponse.json({message:'Image file is required'},{status:400})
+     }
+
+     const arrayBuffer =await file.arrayBuffer();
+     const buffer=Buffer.from(arrayBuffer);
+
+     const uploadResult= await new Promise((resolve,reject)=>{
+      cloudinary.uploader.upload_stream({resource_type:'image', folder:'DevEvent'},(error,results)=>{
+          if(error) return reject(error);
+          resolve(results)
+      }).end(buffer)
+     })
+
+        event.image=(uploadResult as {secure_url:string}).secure_url;
+
     const createdEvent= await Event.create(event);
      return NextResponse.json({message:'Event created succesfully',event:createdEvent},{status:201})
     }
     catch(e){
         console.error(e)
         return NextResponse.json({message:"Event creation failed",error :e instanceof Error ? e.message : 'Unknown'},{status:500})
+    }
+}
+
+export async function GET(){
+    try{
+       await connectDB();
+       const events= await Event.find().sort({createdAt:-1})
+
+        return NextResponse.json({mesaage:'Events fetched successfully',events},{status:200})
+    }
+    catch(e){
+        return NextResponse.json({message:'Event fetching failed',error:e},{status:500})
     }
 }

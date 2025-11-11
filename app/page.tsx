d605bd473944a543cfdd300137ec8bd1328a4144
Raghwendra Kumar <raghwendra.kumar@starhealth.in>
2025-11-12 01:20:40 +0530

    Bug fixes

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import ExploreBtn from "@/components/ExploreBtn";
 import EventCard from "@/components/EventCard";
 import {IEvent} from "@/database";
 import {cacheLife} from "next/cache";
-// import events from "@/lib/constants";
+import events from "@/lib/constants";
 
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 
@@ -23,7 +23,7 @@ export default async function Home() {
           <h3>Featured Events</h3>
 
           <ul className="events">
-              {events && events.length >0 && events.map((event : IEvent)=>(
+              {events && events.length >0 && (events as IEvent[]).map((event : IEvent)=>(
                   <li key={event.title} className='list-none'>
                       <EventCard {...event}
                       />
